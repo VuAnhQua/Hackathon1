e6781b6264b0a56a7c006008fb439f928d6f6b85
Quan <111527496+VuAnhQua@users.noreply.github.com>
2026-05-01 18:07:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import yfinance as yf
 
-msft = yf.Ticker("MSFT")
-# print(msft.info)
-print(msft.financials)
+listOfStock = {"MSFT", "AAPL"}
+listOfStuffToGet = {"dividendRate", "dividendYield", "country", "industry", "dayHigh", "dayLow", "open", "previousClose", "volume", "allTimeHigh", "currentPrice", "targetHighPrice", "targetLowPrice", "recommendationKey", "fiftyTwoWeekRange", "displayName"}
+for x in listOfStock:
+    ticker = yf.Ticker(x)
+    data = ticker.info
+    news = ticker.news
+    # print(data)
+    tempStr = ""
+    for y in listOfStuffToGet:
+        tempStr += y + ": " + str(data[y])
+        if y.find("Yield"):
+            # tempStr += "%"
+            pass
+        tempStr += "\n"
+    print("Stock: " + x + "\n" + tempStr + "\n")
+    print(news)
