aa5fa6578877ee75f4d6a5a550e2f87e56caa895
Quan <111527496+VuAnhQua@users.noreply.github.com>
2026-05-02 01:23:52 -0500

    Update market.py

--- market.py
+++ market.py
@@ -1,7 +1,8 @@
 import yfinance as yf
 
-
-def get_stock_data(ticker_symbol: str, amount: float):
+# Period can only be 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max
+# Interval can only be 1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 4h, 1d, 5d, 1wk, 1mo, 3mo
+def get_stock_data(ticker_symbol: str, amount: float, PeriodRange="1mo", IntervalRange="1d"):
     ticker = yf.Ticker(ticker_symbol)
     data = ticker.info
 
@@ -10,5 +11,9 @@ def get_stock_data(ticker_symbol: str, amount: float):
         "amount": amount,
         "price": data.get("currentPrice", 0),
         "sector": data.get("sector", "Unknown"),
-        "company_name": data.get("displayName", ticker_symbol.upper())
+        "company_name": data.get("displayName", ticker_symbol.upper()),
+        "dividends": data.get("dividendYield"),
+        "past_data": ticker.history(period=PeriodRange, interval=IntervalRange)
     }
+
+# You can get open, volume, high, low data. (Ex: get_stock_data("AAPL", 5, "3mo")["past_data"]["Open"])
